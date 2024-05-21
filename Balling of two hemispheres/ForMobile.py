5f8f5590f38024a1e14621c4981edf481c1f22bf
Pouya Aryani <pabfa.enm@gmail.com>
2024-05-21 10:18:15 +0330

    Add files via upload

--- Balling of two hemispheres/ForMobile.py
+++ Balling of two hemispheres/ForMobile.py
@@ -29,13 +29,23 @@ except:
     pass
 pygame.display.set_caption('Balling of Two hemispheres')  # تنظیم اسم
 
-
+button_up = pygame.Rect(50, 500, 100, 100)
+button_up_image = pygame.image.load('up.png').convert_alpha()
+button_down = pygame.Rect(50, 610, 100, 100)
+button_dn_image = pygame.image.load('dow.png').convert_alpha()
+button_up2 = pygame.Rect(1130, 500, 100, 100)
+button_down2 = pygame.Rect(1130, 610, 100, 100)
 def draw_window(paddle1, paddle2, paddle_center, ball, score, elapsed_time, color, paused):
     win.fill(color)  # پاک کردن صفحه با رنگ متغیر
     pygame.draw.rect(win, (255-color[0], 255-color[1], 255-color[2]), paddle1)  # رسم paddle1
     pygame.draw.rect(win, (255-color[0], 255-color[1], 255-color[2]), paddle2)  # رسم paddle2
     pygame.draw.rect(win, (255-color[0], 255-color[1], 255-color[2]), paddle_center)  # رسم paddle_center
     pygame.draw.ellipse(win, (255-color[0], 255-color[1], 255-color[2]), ball)  # رسم توپ
+
+    win.blit(button_up_image, button_up)
+    win.blit(button_up_image, button_up2)
+    win.blit(button_dn_image, button_down)
+    win.blit(button_dn_image, button_down2)
     if paused:
         score_text = big_font.render("Score: " + str(score), 1, (255-color[0], 255-color[1], 255-color[2]))
         win.blit(score_text, (win_width / 2 - score_text.get_width() / 2, win_height / 2 - score_text.get_height() / 2))
@@ -46,7 +56,7 @@ def draw_window(paddle1, paddle2, paddle_center, ball, score, elapsed_time, colo
         win.blit(time_text, (10, 10))
     pygame.display.update()  # به‌روزرسانی صفحه
 
-ball_vel = 10
+ball_vel = 8
 score = 0
 
 #pyinstaller -w -F -i ICO.ico MainCod.py
@@ -89,18 +99,20 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:  # تغییر وضعیت بازی با دکمه اسپیس
-                    paused = not paused
-                if not paused:
-                    if event.key == pygame.K_w:
-                        paddle1_dy = -paddle_vel
-                    elif event.key == pygame.K_s:
-                        paddle1_dy = paddle_vel
-                    elif event.key == pygame.K_UP:
-                        paddle2_dy = -paddle_vel
-                    elif event.key == pygame.K_DOWN:
-                        paddle2_dy = paddle_vel
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_up.collidepoint(event.pos):
+                    paddle1_dy = -paddle_vel
+                elif button_down.collidepoint(event.pos):
+                    paddle1_dy = paddle_vel
+                if button_up2.collidepoint(event.pos):
+                    paddle2_dy = -paddle_vel
+                elif button_down2.collidepoint(event.pos):
+                    paddle2_dy = paddle_vel
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if button_up.collidepoint(event.pos) or button_down.collidepoint(event.pos):
+                    paddle1_dy = 0
+                if button_up2.collidepoint(event.pos) or button_down2.collidepoint(event.pos):
+                    paddle2_dy = 0
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_s:
                     paddle1_dy = 0
